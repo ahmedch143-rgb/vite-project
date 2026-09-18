41cdc0da79594acca883b4418f494f0bf903cb76
ahmedch143-rgb <ahmedch1433@gmail.com>
2026-09-19 00:12:47 +0500

    Build Super Grill Burger website: update src/navbar/navbar.jsx

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,20 +1,22 @@
 import './navbar.css'
-import ssImage from '../assets/ss.png'
+import logo from '../assets/ss.png'
 
 const Navbar = ({ onSignIn }) => (
   <nav className="navbar">
-    <a className="brand" href="#home" aria-label="The Digital Impacts home">
-      <img src={ssImage} alt="The Digital Impacts logo" />
+    <a className="brand" href="#home" aria-label="Super Grill Burger home">
+      <img src={logo} alt="Super Grill Burger logo" />
+      <span>SUPER <b>GRILL</b></span>
     </a>
+
     <ul className="components">
       <li><a href="#home">Home</a></li>
-      <li><a href="#services">Services</a></li>
-      <li><a href="#about">About Us</a></li>
-      <li><a href="#work-together">Contact</a></li>
+      <li><a href="#menu">Menu</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#contact">Contact</a></li>
     </ul>
+
     <div className="nav-actions">
-      <button type="button" className="btn1 btn-secondary" onClick={onSignIn}>Sign In</button>
-      <a className="btn1" href="#work-together">Contact Us</a>
+      <button type="button" className="order-btn" onClick={onSignIn}>Order Now</button>
     </div>
   </nav>
 )
