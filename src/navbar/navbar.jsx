3e2a59cf5e057c0c20f1cfb4ac3133d8b44fd710
ahmedch143-rgb <ahmedch1433@gmail.com>
2026-09-19 00:08:22 +0500

    Fix React project structure and responsive UI: src/navbar/navbar.jsx

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,28 +1,22 @@
-import React from 'react'
 import './navbar.css'
 import ssImage from '../assets/ss.png'
 
-const Navbar = ({ onSignIn }) => {
-  return (
-    <nav className='navbar'>
-      <div className='img'>
-        <img src={ssImage} alt='ss' />
-      </div>
-
-      <ul className='components'>
-        <li><a href='#home'>Home</a></li>
-        <li><a href='#services'>Services</a></li>
-        <li><a href='#about'>About Us</a></li>
-      </ul>
-
-      <div className='nav-actions'>
-        <button type='button' className='btn1 btn-secondary' onClick={onSignIn}>
-          Sign In
-        </button>
-        <button type='button' className='btn1'>Contact Us</button>
-      </div>
-    </nav>
-  )
-}
+const Navbar = ({ onSignIn }) => (
+  <nav className="navbar">
+    <a className="brand" href="#home" aria-label="The Digital Impacts home">
+      <img src={ssImage} alt="The Digital Impacts logo" />
+    </a>
+    <ul className="components">
+      <li><a href="#home">Home</a></li>
+      <li><a href="#services">Services</a></li>
+      <li><a href="#about">About Us</a></li>
+      <li><a href="#work-together">Contact</a></li>
+    </ul>
+    <div className="nav-actions">
+      <button type="button" className="btn1 btn-secondary" onClick={onSignIn}>Sign In</button>
+      <a className="btn1" href="#work-together">Contact Us</a>
+    </div>
+  </nav>
+)
 
 export default Navbar
