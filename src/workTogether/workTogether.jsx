7b2effebd5e39ce79539cf955652aee2c3e47674
ahmedch143-rgb <ahmedch1433@gmail.com>
2026-09-19 00:08:35 +0500

    Fix React project structure and responsive UI: src/workTogether/workTogether.jsx

--- src/workTogether/workTogether.jsx
+++ src/workTogether/workTogether.jsx
@@ -1,18 +1,11 @@
 import './workTogether.css'
 
-const WorkTogether = () => {
-  return (
-    <section className='work-together-section'>
-      <div className='top-bar'></div>
-      <h2>Let’s Work Together</h2>
-      <p className='subtitle'>
-        Driving your business forward with strong marketing strategies
-        <br />
-        <em>Get a free consultation to make this happen!</em>
-      </p>
-      <button className='connect-btn'>Connect</button>
-    </section>
-  )
-}
-
+const WorkTogether = () => (
+  <section id="work-together" className="work-together-section">
+    <div className="top-bar" />
+    <h2>Let’s Work Together</h2>
+    <p className="subtitle">Driving your business forward with strong marketing strategies<br/><em>Get a free consultation to make this happen!</em></p>
+    <a className="connect-btn" href="mailto:info@thedigitalimpacts.com">Connect</a>
+  </section>
+)
 export default WorkTogether
